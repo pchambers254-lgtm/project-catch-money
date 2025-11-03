4c8170f2ddf3de3f2975661d534cc2d73b2411ba
japneet300 <JSingh0024@colliervilledragons.org>
2025-11-03 14:15:54 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . f f f . . . . . . . 
+    . . . . . . f d f . . . . . . . 
+    . . . . . . f d f f f f f f . . 
+    . f f f f f f d f d d d d f . . 
+    . d d d d d d d d d f f f f . . 
+    . d d d d d d d d d d d d f . . 
+    . d d d d d d d d f f f f f . . 
+    . d d d d f d d d d d d d f . . 
+    . f f f f f d d d f f f f f . . 
+    . . . . . f d d d d d d d f . . 
+    . . . . . f f f f f f f f f . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
